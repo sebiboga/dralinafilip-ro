2980659b464aed40e64b249febd6ecb29e05d0bc
talidag <talida.ganciu@gmail.com>
2024-06-18 15:05:25 +0300

    first part of mobile responsiveness

--- script.js
+++ script.js
@@ -37,3 +37,35 @@ function mySubmitFunction(e) {
 
   return false;
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburgerMenuIcon = document.querySelector(".hamburger-menu-icon");
+  const closeMenuIcon = document.querySelector(".close-menu-icon");
+  const navLinksList = document.querySelector(".nav__links__list");
+  const navLinks = document.querySelectorAll(".nav__links__list a");
+  const logoEl = this.documentElement.querySelector(".nav-logo-container");
+
+  function closeMenu() {
+    navLinksList.style.display = "none";
+    hamburgerMenuIcon.style.display = "block";
+    closeMenuIcon.style.display = "none";
+  }
+
+  hamburgerMenuIcon.addEventListener("click", function () {
+    navLinksList.style.display = "flex";
+    hamburgerMenuIcon.style.display = "none";
+    closeMenuIcon.style.display = "block";
+  });
+
+  closeMenuIcon.addEventListener("click", function () {
+    navLinksList.style.display = "none";
+    hamburgerMenuIcon.style.display = "block";
+    closeMenuIcon.style.display = "none";
+  });
+
+  navLinks.forEach((link) => {
+    link.addEventListener("click", closeMenu);
+  });
+
+  logoEl.addEventListener("click", closeMenu);
+});
