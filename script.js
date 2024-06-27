c26d311d5e745f422b99587c69ecf3261b28848d
talidag <talida.ganciu@gmail.com>
2024-06-27 13:04:15 +0300

    swiper JS responsiveness and questions arrows function

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ document.addEventListener("DOMContentLoaded", function () {
   const swiper = new Swiper(".mySwiper", {
     slidesPerView: 3,
     spaceBetween: 32,
+    slideToClickedSlide: true,
+
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
@@ -17,28 +19,31 @@ document.addEventListener("DOMContentLoaded", function () {
     },
     on: {
       init: function () {
-        updateActiveSlide(this);
+        console.log("Swiper initialized");
       },
-      slideChange: function () {
-        updateActiveSlide(this);
+      touchStart: function () {
+        console.log("Touch start");
+      },
+      touchEnd: function () {
+        console.log("Touch end");
+      },
+      // Add more events as needed
+    },
+    breakpoints: {
+      1250: {
+        slidesPerView: 3,
+        spaceBetween: 120,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+      0: {
+        slidesPerView: 1,
+        spaceBetween: 10,
       },
     },
   });
-
-  function updateActiveSlide(swiper) {
-    const slides = swiper.slides;
-    const activeIndex = swiper.activeIndex;
-    const centerIndex =
-      activeIndex + Math.floor(swiper.params.slidesPerView / 2);
-
-    slides.forEach((slide, index) => {
-      slide.classList.remove("active-slide");
-    });
-
-    if (slides[centerIndex]) {
-      slides[centerIndex].classList.add("active-slide");
-    }
-  }
 });
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -114,3 +119,24 @@ document.addEventListener("DOMContentLoaded", function () {
 
   logoEl.addEventListener("click", closeMenu);
 });
+
+const intrebariCards = document.querySelectorAll(".intrebari__card");
+
+intrebariCards.forEach((card) => {
+  const rightArrowQ = card.querySelector(".arrow-right-q");
+  const downArrowQ = card.querySelector(".arrow-down-q");
+  const intrebariAnswer = card.querySelector(".intrebari-answer");
+  rightArrowQ.addEventListener("click", () => {
+    toggleArrows(rightArrowQ, downArrowQ, intrebariAnswer);
+  });
+
+  downArrowQ.addEventListener("click", () => {
+    toggleArrows(rightArrowQ, downArrowQ, intrebariAnswer);
+  });
+});
+
+const toggleArrows = (rightArrow, downArrow, intrebariAnswer) => {
+  rightArrow.classList.toggle("invisible");
+  downArrow.classList.toggle("invisible");
+  intrebariAnswer.classList.toggle("invisible");
+};
